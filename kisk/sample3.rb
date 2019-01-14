741362cc08d866edd69374ddc71088b266703166
Yoshimitsu Kiyono <yoshi.kiyono@gmail.com>
2019-01-14 14:37:30 +0900

    step3

---
+++ kisk/sample3.rb
@@ -0,0 +1,21 @@
+# sample3
+
+# 0
+puts [1, 2].inject(:+)
+
+# 1
+a = 1
+b = 2
+
+puts a * b
+
+# 2
+puts [a, b].inject(:+)
+
+# 3
+SHOHIZEI = 0.1
+shoukei = 1000
+
+goukei = shoukei + (shoukei * SHOHIZEI)
+
+puts goukei
